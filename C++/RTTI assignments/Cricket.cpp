1426599b15346f3eef1b39d5778c25ff2d973b5b
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-05-16 23:21:24 +0530

    Create Cricket.cpp

---
+++ C++/RTTI assignments/Cricket.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+class Cricket {
+public:
+    virtual void play() = 0;
+};
+
+class FiftyOver : public Cricket {
+public:
+    void play() {
+        cout << "Lets play FiftyOver cricket" << endl;
+    }
+};
+
+class Test : public Cricket {
+public:
+    void play() {
+        cout << "Lets play Test cricket" << endl;
+    }
+
+    void daywise_summary() {
+        cout << "Test match day wise summary" << endl;
+    }
+};
+
+class TwentyOver : public Cricket {
+public:
+    void play() {
+        cout << "Lets play TwentyOver cricket" << endl;
+    }
+
+};
+
+//void doit(Cricket &ptr) {
+//    ptr.play();
+//    try {
+//        Test &rf = dynamic_cast<Test &>(ptr);
+//        rf.daywise_summary();
+//    }
+//    catch (bad_cast &bc) {
+//        cout << "Error is: " << bc.what() << endl;
+//    }
+//}
+
+int main() {
+    Cricket *ptr[]={new FiftyOver,new Test,new TwentyOver};
+    for (int i = 0; i < 3; ++i) {
+        //ptr[i]->play();
+        Test* rf=dynamic_cast<Test*>(ptr[i]);
+                if(rf){
+                    ptr[i]->play();
+                    rf->daywise_summary();
+                }
+    }
+    return 0;
+}
+
