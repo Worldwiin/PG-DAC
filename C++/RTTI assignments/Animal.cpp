3f6ee0a3bca1dd500c8c0e6b1e1857eadcf119ad
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-05-17 15:58:47 +0530

    Update Animal.cpp

--- C++/RTTI assignments/Animal.cpp
+++ C++/RTTI assignments/Animal.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
 
+using namespace std;
+
+class Animal {
+public:
+    virtual void Makesound()=0;
+};
+
+class Dog : public Animal {
+public:
+    void Makesound() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void Makesound() {
+        cout << "Cat meows" << endl;
+    }
+};
+
+class Tiger : public Animal {
+public:
+    void Makesound() {
+        cout << "Tiger roars" << endl;
+    }
+
+    void hunting() {
+        cout << "Tiger getting hunted" << endl;
+    }
+};
+
+void perform(Animal *ptr) {
+    Tiger *rf = dynamic_cast<Tiger *>(ptr);
+    if (rf) {
+        rf->Makesound();
+        rf->hunting();
+    }
+}
+
+int main() {
+    perform(new Dog);
+    perform(new Cat);
+    perform(new Tiger);
+    return 0;
+}
