3d194cc76e18f92011871fc2e4526cfa48ef9b71
Worldwin <worldwin93@gmail.com>
2024-07-28 13:38:44 +0530

    Pattern problem

---
+++ Pattern_problems/Pattern_4.java
@@ -0,0 +1,14 @@
+public class Pattern_4 {
+    public static void main(String[] args) {
+        int n = 69;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 65; j <= n; j++) {
+                for (int k = j; k <= 69; k++) {
+                    System.out.print((char)k);
+                }
+            }
+            n = n - 1;
+            System.out.println();
+        }
+    }
+}
