b50710c1baf4b0accf4eaa37aeaa2837e565663c
Worldwin <worldwin93@gmail.com>
2024-07-28 13:01:52 +0530

    Pattern problems

---
+++ Pattern_problems/Pattern_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern_1 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int s = i; s <= n - 1; s++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
