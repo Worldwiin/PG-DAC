84874320562ee7356b9e84659607842fb78b7a69
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-08-25 13:04:17 +0530

    file name change

--- Logic/String_sort_within_words.java
+++ Logic/String_sort_within_words.java
@@ -1,4 +1,4 @@
-public class String_words_within_sort {
+public class String_sort_within_words {
     public static void main(String[] args) {
         String s1 = "HELLO WORLD";
         String[] words = s1.split(" ");
