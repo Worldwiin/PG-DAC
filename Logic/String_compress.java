da1a1f62bd9e508640a1b365c84af4eb7a3362bb
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-08-26 06:47:04 +0530

    some changes

---
+++ Logic/String_compress.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class String_compress {
+
+    public static String compressString(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        StringBuilder compressed = new StringBuilder();
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : input.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            compressed.append(entry.getKey());
+            compressed.append(entry.getValue());
+        }
+
+        if (compressed.length() < input.length()) {
+            return compressed.toString();
+        } else {
+            return input;
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "aabcccccaaa";
+        String compressed = compressString(input);
+        System.out.println("Compressed String: " + compressed);
+    }
+}
+//Input: aabcccccaaa
+//Output: a5b1c5
