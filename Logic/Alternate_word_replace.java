a17faac4379e8c7107d36335ec454d795aecd159
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-08-30 12:56:51 +0530

    removed inbuilt string methods

--- Logic/Alternate_word_replace.java
+++ Logic/Alternate_word_replace.java
@@ -1,15 +1,42 @@
 public class Alternate_word_replace {
-    public static void main (String [] args){
-        String str="Virat.is.good.batsman.";
-        String[] arr=str.split("\\.");
-        String replace="xyz";
-//        System.out.println(arr.length);
-        for(int i=1;i<=arr.length;i+=2){
-            arr[i]=replace;
+    public static void main(String[] args) {
+        String a = "Virat.is.good.batsman";
+        String word = "";
+        String finalStr = "";
+        int wordCount = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != '.') {
+                word += a.charAt(i);
+            } else {
+                if (wordCount % 2 == 0) {
+                    finalStr += word + ".";
+                } else {
+                    finalStr += "xyz.";
+                }
+                word = "";
+                wordCount++;
+            }
         }
-        for(int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+".");
+        if (wordCount % 2 == 0) {
+            finalStr += word;
+        } else {
+            finalStr += "xyz";
         }
+
+        System.out.println(finalStr);
     }
 }
 //Op: Virat.xyz.good.xyz.
+
+//         String str="Virat.is.good.batsman.";
+//        String[] arr=str.split("\\.");
+//        String replace="xyz";
+////        System.out.println(arr.length);
+//        for(int i=1;i<=arr.length;i+=2){
+//            arr[i]=replace;
+//        }
+//        for(int i=0;i< arr.length;i++){
+//            System.out.print(arr[i]+".");
+//        }
+
+
